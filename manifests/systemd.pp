241a0a3bb19f670db98a9c1bca3361f89b2b4cb1
Les Aker <me@lesaker.org>
2022-05-09 21:25:43 -0400

    modify grub config

--- manifests/systemd.pp
+++ manifests/systemd.pp
@@ -11,5 +11,34 @@ class console::systemd () {
       ensure => running,
       enable => true,
     }
+
+    file_line { 'grub_input':
+      ensure => present,
+      path   => '/etc/default/grub',
+      line   => 'GRUB_TERMINAL_INPUT="serial"',
+      match  => '^GRUB_TERMINAL_INPUT=',
+      notify => Exec['update grub'],
+    }
+
+    file_line { 'grub_output':
+      ensure => present,
+      path   => '/etc/default/grub',
+      line   => 'GRUB_TERMINAL_OUTPUT="serial"',
+      match  => '^GRUB_TERMINAL_OUTPUT=',
+      notify => Exec['update grub'],
+    }
+
+    file_line { 'grub_serial':
+      ensure => present,
+      path   => '/etc/default/grub',
+      line   => 'GRUB_SERIAL_COMMAND="serial --unit=0 --speed=115200"',
+      match  => '^GRUB_SERIAL_COMMAND=',
+      notify => Exec['update grub'],
+    }
+
+    exec { 'update grub':
+      command     => '/usr/bin/grub-mkconfig -o /boot/grub/grub.cfg',
+      refreshonly => true,
+    }
   }
 }
